96cc4b661491a22f10e230ffb143acc5f4248afc
bwierzbo <bwierzbo@gmail.com>
2025-07-07 18:51:34 -0400

    Initial Commit

---
+++ BumpSetCut/BumpSetCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumpSetCutApp.swift
+//  BumpSetCut
+//
+//  Created by Benjamin Wierzbanowski on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BumpSetCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BumpSetCutTests/BumpSetCutTests.swift
@@ -0,0 +1,17 @@
+//
+//  BumpSetCutTests.swift
+//  BumpSetCutTests
+//
+//  Created by Benjamin Wierzbanowski on 7/7/25.
+//
+
+import Testing
+@testable import BumpSetCut
+
+struct BumpSetCutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
